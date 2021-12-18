d422c8e7fd17eb557d82a7f4220198304a6fe652
aquablue31 <90348603+aquablue31@users.noreply.github.com>
2021-12-17 20:37:09 -0500

    Create player.java

---
+++ PROJECT3/player.java
@@ -0,0 +1,24 @@
+// player file
+
+public class Player {
+  private int lives;
+  
+  public Player() {
+    this.lives = 5;
+  }
+  
+  public Player(int live) {
+    this.lives = live;
+  }
+  
+  public int getLives() {
+    return this.lives;
+  }
+  
+  public void death() {
+    this.lives--;
+  }
+}
+
+
+// LIVES / DEATH. 
